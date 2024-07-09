8b703c3c2cbe6cd1d8d0eeb949db9e10eb0a5fc0
Jagan-2K1 <jagannath@vivyacorp.com>
2024-07-09 16:26:34 +0530

    Checking CICD

--- cicd_pro/settings.py
+++ cicd_pro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=&6d+*4eo7wle+#3&@@mc%&!!ne4363lwl#j63t#i$ddg#54ae
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'cicd_pro.urls'
 
 TEMPLATES = [
