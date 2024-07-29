21e4902b5c8ec8e7db9a5ff26a64cad2e52c3e32
Jagan-2K1 <jagannath@vivyacorp.com>
2024-07-29 15:26:13 +0530

    Checking yaml  CICD

--- cicd_pro/settings.py
+++ cicd_pro/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+AFDG
 # SWAGGER_SETTINGS = {
 #     'DEFAULT_INFO': 'path.to.urls.api_info',
 # }
