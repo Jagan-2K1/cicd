cda08c22d3a3182425c0292032032db18571d07c
Jagan-2K1 <jagannath@vivyacorp.com>
2024-07-29 15:18:30 +0530

    Checking yaml  CICD

--- .github/workflows/mail.py
+++ .github/workflows/mail.py
@@ -32,3 +32,58 @@ try:
     print('Mail Sent Successfully!')
 except Exception as e:
     print(f'Failed to send email: {e}')
+
+
+
+#
+# name: CHECKING SERVER STATUS
+# on:
+#   push:
+#     branches:
+#       - jagannath
+#
+# concurrency:
+#   group: jagannath
+#   cancel-in-progress: true
+#
+# jobs:
+#   deploy:
+#     name: Continuous Integration
+#     uses: ./.github/workflows/master.yml
+#     with:
+#       SSH_PRIVATE_KEY: ${{ secrets.SSH_PRIVATE_KEY }}
+#       SSH_HOST: ${{ secrets.SSH_HOST }}
+#       SSH_USER: ${{ secrets.SSH_USER }}
+#
+#   mail-service:
+#     name: Mail Sending
+#     needs: deploy
+#     runs-on: ubuntu-latest
+#     steps:
+#       - name: Configure SSH
+#         env:
+#           SSH_PRIVATE_KEY: ${{ secrets.SSH_PRIVATE_KEY }}
+#           SSH_HOST: ${{ secrets.SSH_HOST }}
+#           SSH_USER: ${{ secrets.SSH_USER }}
+#         run: |
+#           mkdir -p ~/.ssh/
+#           echo "$SSH_PRIVATE_KEY" > ~/.ssh/github
+#           chmod 600 ~/.ssh/github
+#           cat >> ~/.ssh/config <<END
+#           Host target
+#             HostName $SSH_HOST
+#             User $SSH_USER
+#             IdentityFile ~/.ssh/github
+#             LogLevel ERROR
+#             StrictHostKeyChecking no
+#           END
+#
+#       - name: Checking server Status
+#         run: |
+#           ps aux | grep 'python manage.py runserver 0.0.0.0:2914' | grep -v grep
+#         id: check_server_status
+#
+#       - name: Print server status
+#         run: echo "${{ steps.check_server_status.outputs }}"
+#
+
